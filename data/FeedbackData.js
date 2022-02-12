f166b2c7cebcd394435eebbc06cc1fdfe6dae0fe
ANKITA SETH <97723029+annie790@users.noreply.github.com>
2022-02-12 13:42:56 +0530

    Add files via upload

---
+++ data/FeedbackData.js
@@ -0,0 +1,19 @@
+const FeedbackData = [
+    {
+    id: 1,
+    rating: 10,
+    text: 'hey',
+},
+{
+    id: 2,
+    rating: 9,
+    text: 'hey',
+},
+{
+    id: 3,
+    rating: 8,
+    text: 'hey',
+},
+]
+
+export default FeedbackData
